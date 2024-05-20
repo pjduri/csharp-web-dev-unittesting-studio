3b9973092054b3e32526771b7ee5391bfbf074fa
Your Name <108307350+pjduri@users.noreply.github.com>
2024-05-20 11:53:07 -0500

    added comments

--- BalancedBracketsTests/BalancedBracketsTests.cs
+++ BalancedBracketsTests/BalancedBracketsTests.cs
@@ -6,91 +6,105 @@ public class BalancedBracketsTests
 {
     // TODO: Add tests to this file.
     // POSITIVE CASE TESTS
+    // "" should return true
     [TestMethod]
     public void EmptyStringReturnsTrue()
     {
-        string message = "'' should return true";
+        string message = "empty string should return true";
         IsTrue(BalancedBrackets.HasBalancedBrackets(""), message);
     }
 
+    // "[]" should return true
     [TestMethod]
     public void OnlyBracketsReturnsTrue()
     {
-        string message = "'[]' should return true";
+        string message = "single set of balanced brackets returns true";
         IsTrue(BalancedBrackets.HasBalancedBrackets("[]"), message);
     }
 
+    // '[LaunchCode]' should return true
     [TestMethod]
     public void BracketsAroundStringReturnsTrue()
     {
-        string message = "'[LaunchCode]' should return true";
+        string message = "balanced brackets around a string should return true";
         IsTrue(BalancedBrackets.HasBalancedBrackets("[LaunchCode]"), message);
     }
 
+    // "[]LaunchCode", "LaunchCode[]", and "[]LaunchCode[] return true
     [TestMethod]
-    public void BracketsBeforeStringReturnsTrue()
+    public void BracketsBeforeAndAfterStringReturnsTrue()
     {
-        string message = "'[]LaunchCode' should return true";
+        string message = "brackets before and/or after string returns true";
         IsTrue(BalancedBrackets.HasBalancedBrackets("[]LaunchCode"), message);
+        IsTrue(BalancedBrackets.HasBalancedBrackets("LaunchCode[]"), message);
+        IsTrue(BalancedBrackets.HasBalancedBrackets("[]LaunchCode[]"), message);
     }
 
+    // [][][] returns true
     [TestMethod]
     public void ThreeSetsBracketsReturnsTrue()
     {
-        string message = "'[][][]' should return true";
+        string message = "multiple balanced pairs returns true";
         IsTrue(BalancedBrackets.HasBalancedBrackets("[][][]"), message);
     }
 
+    // [[]] returns true
     [TestMethod]
     public void NestedBracketsReturnsTrue()
     {
-        string message = "'[[]]' should return true";
+        string message = "nested brackets return true";
         IsTrue(BalancedBrackets.HasBalancedBrackets("[[]]"), message);
     }
 
+    // [L[a[un]c[hCo]de]] returns true
     [TestMethod]
     public void NestedBracketsWithinStringReturnsTrue()
     {
-        string message = "'[L[a[un]c[hCo]de]]' should return true";
+        string message = "nested brackets in string returns true";
         string testString = "[L[a[un]c[hCo]de]]";
         IsTrue(BalancedBrackets.HasBalancedBrackets(testString), message);
     }
 
     // NEGATIVE CASE TESTS
+    // [ and ] alone should return false
     [TestMethod]
     public void OnlyOneBracketReturnsFalse()
     {
-        string message = "'[' and ']' should both return false";
+        string message = "single brackets should return false";
         IsFalse(BalancedBrackets.HasBalancedBrackets("["), message);
         IsFalse(BalancedBrackets.HasBalancedBrackets("]"), message);
     }
 
+    // '[[]' and '[]]' should both return false
     [TestMethod]
     public void MoreOfOneBracketReturnsFalse()
     {
-        string message = "'[[]' and '[]]' should both return false";
+        string message = "more of one bracket type should return false";
         IsFalse(BalancedBrackets.HasBalancedBrackets("[[]"), message);
         IsFalse(BalancedBrackets.HasBalancedBrackets("[]]"), message);
     }
     
+    // '][' should return false
     [TestMethod]
-    public void ClosingBracketFirstReturnsFalse()
+    public void WrongOrderBracketsReturnsFalse()
     {
-        string message = "'][' should return false";
+        string message = "closing bracket before opening returns false";
         IsFalse(BalancedBrackets.HasBalancedBrackets("]["), message);
     }
 
+    // '[]][[]' should return false
     [TestMethod]
     public void MisNestedBracketsReturnsFalse()
     {
-        string message = "'[]][[]' should return false";
+        string message = "any closing bracket before its opener returns false";
         IsFalse(BalancedBrackets.HasBalancedBrackets("[]][[]"), message);
     }
 
+    // '[L]au]nch[co[d]e' should return false
     [TestMethod]
     public void MisNestedBracketsInStringReturnsFalse()
     {
-        string message = "'[L]au]nch[co[d]e' should return false";
+        string message = "any closer before its opener in a string returns false";
         IsFalse(BalancedBrackets.HasBalancedBrackets("[L]au]nch[co[d]e"), message);
     }
 }
